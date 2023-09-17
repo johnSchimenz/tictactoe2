6fda351fbca0b4362d3d175967161f35d1082ea4
John Schimenz <john.schimenz@gmail.com>
2023-09-17 14:04:35 -0500

    Console.log displayes winner or tie

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const gameBoard = (() => {
 })();
 
 // Player factory
-const PlayerFactory = (playerName, marker) => {
+const PlayerFactory = (name, marker) => {
     winner = false;
-    return {playerName, marker, winner};
+    return {name, marker, winner};
 };
 
 // gameSetUp module
@@ -24,31 +24,73 @@ const gameSetUp = (() => {
     };
 })();
 
+// checkWinner function
+function checkWinner(player, marker) {
+    if (
+        gameBoardArray[0] === marker && gameBoardArray[1] == marker && gameBoardArray[2] === marker ||
+        gameBoardArray[3] === marker && gameBoardArray[4] == marker && gameBoardArray[5] === marker ||
+        gameBoardArray[6] === marker && gameBoardArray[7] == marker && gameBoardArray[8] === marker ||
+        gameBoardArray[0] === marker && gameBoardArray[3] == marker && gameBoardArray[6] === marker ||
+        gameBoardArray[1] === marker && gameBoardArray[4] == marker && gameBoardArray[7] === marker ||
+        gameBoardArray[2] === marker && gameBoardArray[5] == marker && gameBoardArray[8] === marker ||
+        gameBoardArray[0] === marker && gameBoardArray[4] == marker && gameBoardArray[8] === marker ||
+        gameBoardArray[2] === marker && gameBoardArray[4] == marker && gameBoardArray[6] === marker
+        ) {
+            player.winner = true;
+            console.log(player);
+            return player;
+        }
+}
+
+// displayCurrentResults
+function displayCurrentResults(playerOne, playerTwo, roundCounter) {
+    if (playerOne.winner === true) {
+        console.log(playerOne.name + " is the winner!  Play again?");
+    }
+    if (playerTwo.winner === true) {
+        console.log(playerTwo.name + " is the winner!  Play again?");
+    }
+    if (playerOne.winner === false && playerTwo.winner === false && roundCounter >=10) {
+        console.log("Tie game.  Play again?");
+    }
+}
+
 // playRound module
 const playRound = (() => {
-    // Make cells of tic-tac-toe board clickable - not sure if this needs to be in a module or factory
+    // Make cells of tic-tac-toe board clickable
     const cells = document.querySelectorAll("button.cell");
     cells.forEach((cell) => {
         cell.addEventListener("click", () => {
             // Get clicked cell's id
             let elementId = cell.getAttribute("id");
 
-            // Make cell display current player's marker and add 1 to roundCounter
+            // Initialize currentPlayer
+            let currentPlayer = "";
+
+            // Make cell display current player's marker, add 1 to roundCounter, and check if we have a winner
             if (gameSetUp.roundCounter % 2 === 1 && cell.textContent === "") {
-                cell.textContent = gameSetUp.playerOne.marker;
+                currentPlayer = gameSetUp.playerOne;
+                cell.textContent = currentPlayer.marker;
                 gameSetUp.roundCounter++;
             }
             else if (gameSetUp.roundCounter % 2 === 0 && cell.textContent === "") {
-                cell.textContent = gameSetUp.playerTwo.marker;
+                currentPlayer = gameSetUp.playerTwo;
+                cell.textContent = currentPlayer.marker;
                 gameSetUp.roundCounter++;
             }
             else {
-                alert("I've already been chosen.  Choose an empty cell");
+                console.log("doesn't work");
             }
 
             // Change gameBoardArray to reflect the current player's choice
             let idNumber = elementId.slice(4, 5);
             gameBoardArray[idNumber] = cell.textContent;
+
+            // Check if there is a winner
+            checkWinner(currentPlayer, currentPlayer.marker);
+
+            // Display what to do next
+            displayCurrentResults(gameSetUp.playerOne, gameSetUp.playerTwo, gameSetUp.roundCounter);
         });
     });
 })();
