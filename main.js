89008a9cd6fc39621d2f1379b61c221b9ce2798e
John Schimenz <john.schimenz@gmail.com>
2023-09-17 13:07:48 -0500

    Selecting spots on the board now works in local scope

--- main.js
+++ main.js
@@ -24,41 +24,31 @@ const gameSetUp = (() => {
     };
 })();
 
-// Make cells of tic-tac-toe board clickable - not sure if this needs to be in a module or factory
-const cells = document.querySelectorAll("button.cell");
-cells.forEach((cell) => {
-    cell.addEventListener("click", () => {
-        // Get clicked cell's id
-        let elementId = cell.getAttribute("id");
+// playRound module
+const playRound = (() => {
+    // Make cells of tic-tac-toe board clickable - not sure if this needs to be in a module or factory
+    const cells = document.querySelectorAll("button.cell");
+    cells.forEach((cell) => {
+        cell.addEventListener("click", () => {
+            // Get clicked cell's id
+            let elementId = cell.getAttribute("id");
 
-        // Make cell display current player's marker and add 1 to roundCounter
-        if (gameSetUp.roundCounter % 2 === 1 && cell.textContent === "") {
-            cell.textContent = gameSetUp.playerOne.marker;
-            gameSetUp.roundCounter++;
-        }
-        else if (gameSetUp.roundCounter % 2 === 0 && cell.textContent === "") {
-            cell.textContent = gameSetUp.playerTwo.marker;
-            gameSetUp.roundCounter++;
-        }
-        else {
-            alert("I've already been chosen.  Choose an empty cell");
-        }
+            // Make cell display current player's marker and add 1 to roundCounter
+            if (gameSetUp.roundCounter % 2 === 1 && cell.textContent === "") {
+                cell.textContent = gameSetUp.playerOne.marker;
+                gameSetUp.roundCounter++;
+            }
+            else if (gameSetUp.roundCounter % 2 === 0 && cell.textContent === "") {
+                cell.textContent = gameSetUp.playerTwo.marker;
+                gameSetUp.roundCounter++;
+            }
+            else {
+                alert("I've already been chosen.  Choose an empty cell");
+            }
 
-        // Change gameBoardArray to reflect the current player's choice
-        let idNumber = elementId.slice(4, 5);
-        gameBoardArray[idNumber] = cell.textContent;
+            // Change gameBoardArray to reflect the current player's choice
+            let idNumber = elementId.slice(4, 5);
+            gameBoardArray[idNumber] = cell.textContent;
+        });
     });
-});
-
-function playRound() {
-    // Determine who is the current player
-    const currentPlayer = ""
-    if (roundCounter % 2 === 1) {
-        currentPlayer = playerOne;
-    }
-    if (roundCounter % 2 === 0) {
-        currentPlayer = playerTwo;
-    }
-
-
-}
+})();
