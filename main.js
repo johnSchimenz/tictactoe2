2b9f3046c6e574a769a600cfeb76600d45f0ff60
John Schimenz <john.schimenz@gmail.com>
2023-09-17 08:37:30 -0500

    Game now creates 2 pre-determined players

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ const PlayerFactory = (playerName, marker) => {
     winner = false;
     return {playerName, marker, winner};
 };
+
+// gameSetUp module
+const gameSetUp = (() => {
+    // Create players
+    const playerOne = PlayerFactory("Abby", "X");
+    const playerTwo = PlayerFactory("Bob", "O");
+
+    return {
+        playerOne,
+        playerTwo,
+    };
+})();
+
+console.log(gameSetUp.playerOne);
+console.log(gameSetUp.playerTwo);
